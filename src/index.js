077b02bcf9d54ccb994588311c45c7cd9f654ca2
Arnav Thorat <62457196+arnavthorat78@users.noreply.github.com>
2023-09-08 03:15:35 +0000

    Started importing classes in `index.js` file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello, world!");
+const logInBtn = document.querySelector(".log-in");
+const signUpBtn = document.querySelector(".sign-up");
+
+const tableInv = document.querySelector(".table-inv");
