128b41d8fd0a110bed6ae473fbcd8cc6adf6e4a0
arnavthorat78 <arnavthorat78@gmail.com>
2023-09-09 16:09:21 +1000

    Added code to fetch data from Firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-const logInBtn = document.querySelector(".log-in");
-const signUpBtn = document.querySelector(".sign-up");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
 
-const tableInv = document.querySelector(".table-inv");
+const firebaseConfig = {
+  apiKey: "AIzaSyAKJQGzVJrLgS4q0dUTVXQMjIkIyS1g7mw",
+  authDomain: "medicine-inventory-a9853.firebaseapp.com",
+  projectId: "medicine-inventory-a9853",
+  storageBucket: "medicine-inventory-a9853.appspot.com",
+  messagingSenderId: "479781032055",
+  appId: "1:479781032055:web:62b1d1f714e06967733672",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const colRef = collection(db, "medicine");
+getDocs(colRef)
+  .then((snapshot) => {
+    const medicines = [];
+
+    snapshot.docs.forEach((medicine) => {
+      medicines.push({ ...medicine.data(), id: medicine.id });
+    });
+
+    console.log(medicines);
+  })
+  .catch((err) => console.log);
